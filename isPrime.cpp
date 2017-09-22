b4be01d725f8469f2f4449660f591fcf5814fd90
cdarais <christopherdarais@icloud.com>
2017-09-22 14:17:08 -0700

    so many primes

--- isPrime.cpp
+++ isPrime.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 
 bool checkPrime(int a){
-	for (int i = 2; i <= a / 2; i++) if (a % i == 0) return false;
-	return true;
+	bool prime = true;
+	for (int i = 2; i <= a / 2; i++) 
+		if (a % i == 0){
+			std::cout << "factor: " << i << std::endl; 
+			prime = false;
+		}
+	return prime;
 }
 
 
 int main(){
-
+	
+	bool prime;
 	int a;
 	
 	std::cout << "enter the number: ";
 	std::cin >> a;
-
-	std::cout << checkPrime(a) << std::endl;
+	
+	prime = checkPrime(a);
+	
+	if(prime)
+		std::cout << "is prime\n\n";
+	else
+		std::cout << "not prime\n\n";
 }
 
